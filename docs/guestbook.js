537359177c730e3b598974aa8068b67a57b83fce
Hugo-Jean EGU <hugoj.egu@gmail.com>
2019-07-26 11:55:57 +0200

    Update guestbook.js

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://9c3609ce.eu-gb.apiconnect.appdomain.cloud/guestbook';
+const apiUrl = 'https://e31b9598.eu-gb.apiconnect.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
